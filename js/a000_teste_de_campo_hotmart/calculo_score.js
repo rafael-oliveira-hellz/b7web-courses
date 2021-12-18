8348c0a94a8a1c398c0c07f4fcd9be331127be89
Rafael Oliveira <rdeoliveirasilva3@gmail.com>
2021-12-18 10:22:50 -0300

    Added first HTML-CSS-JS files

---
+++ js/a000_teste_de_campo_hotmart/calculo_score.js
@@ -0,0 +1,79 @@
+// const currentSheet = ;
+
+var c, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ax, ay;
+var somaScore;
+
+function ScoreCalc(cell) {
+    switch (cell) {
+        case n:
+            if (n) {
+                somaScore += 100;
+            }
+            break;
+        case o:
+            somaScore += 100;
+            break;
+        case p:
+            somaScore += 100;
+            break;
+        case q:
+            somaScore += 100;
+            break;
+        case r:
+            somaScore += 100;
+            break;
+        case s:
+            somaScore += 100;
+            break;
+        case t:
+            somaScore += 100;
+        case u:
+            somaScore += 100;
+            break;
+        case v:
+            somaScore += 100;
+            break;
+        case w:
+            somaScore += 100;
+            break;
+        case x:
+            somaScore += 100;
+            break;
+        case y:
+            somaScore += 100;
+            break;
+        case z:
+            somaScore += 100;
+            break;
+        case aa:
+            somaScore += 100;
+            break;
+        case ab:
+            somaScore += 100;
+            break;
+        case ac:
+            somaScore += 100;
+            break;
+        case ad:
+            somaScore += 100;
+            break;
+        case ae:
+            somaScore += 100;
+            break;
+        case af:
+            somaScore += 100;
+            break;
+        case ag:
+            somaScore += 100;
+            break;
+        case ah:
+            somaScore += 100;
+            break;
+        case ax:
+            somaScore += 100;
+            break;
+        case ay:
+            somaScore += 100;
+            break;
+    }
+};
